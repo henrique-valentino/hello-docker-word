67b239edf5d9c7a2ee21be9bcc7bd0ed651bec74
henrique-valentino <henrique.valentino@gmail.com>
2021-02-16 09:53:03 -0300

    Create project

---
+++ src/main/java/com/example/hellodockerword/HelloDockerWordApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellodockerword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDockerWordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloDockerWordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellodockerword/HelloDockerWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellodockerword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDockerWordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
